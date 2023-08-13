5d9d9dad057f751d88d1b11ce82ebf19c4458eb8
diptanshuG <diptanshubhawsar50@gmail.com>
2023-08-14 03:28:28 +0530

    update ui

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -6,22 +6,23 @@ import { Button } from "react-bootstrap";
 const HomePage = () => {
   return (
     <div className="hero-section">
-      <section>
+      <section className="content-container">
         <div className="slider-container">
           <ImageSlider />
         </div>
-        <div className="text-container">
-          <h2>Custom – Character – Class.</h2>
-          <p>
-            Fashion is like eating, you shouldn't stick to the same menu.
-          </p>
-          <p> Don’t be
-            into trends. Don’t make fashion own you, but you decide what you are,
-            what you want to express by the way you dress and the way you live.</p>
-          <Button variant="primary">Rent Now</Button>
+        <div className="text-container m-5">
+          <div>
+            <h2>Custom – Character – Class.</h2>
+            <p>Fashion is like eating, you shouldn't stick to the same menu.</p>
+            <p>
+              Don’t be into trends. Don’t make fashion own you, but you decide
+              what you are, what you want to express by the way you dress and the
+              way you live.
+            </p>
+            <Button variant="primary">Rent Now</Button>
+          </div>
         </div>
       </section>
-
     </div>
   );
 };
