31ff65aefd1f78a4f1d63e878724cc6980bc8b6d
yashikajain0312 <yashikajain401@gmail.com>
2023-08-13 15:05:28 +0530

    creating hero section

--- client/src/components/HomePage/NavbarComponent.js
+++ client/src/components/HomePage/NavbarComponent.js
@@ -1,65 +1,67 @@
-import React from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap';
-import { FaUser, FaSearch, FaShoppingCart } from 'react-icons/fa';
-import '../../styles/NavbarComponent.css';
-import log from "../../assets/images/logo.png"
-import Categories from './Categories';
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { FaUser, FaSearch, FaShoppingCart } from "react-icons/fa";
+import "../../styles/NavbarComponent.css";
+import log from "../../assets/images/logo2.png";
+import Categories from "./Categories";
 
 const NavbarComponent = () => {
-    const womenItemsType = [
-        "Bridal Lehenga",
-        "Designer Lehenga",
-        "Gown",
-      ];
+  const womenItems = ["Bridal Lehenga", "Designer Lehenga", "Gown"];
 
-      const occasionWear = [
-        "Wedding",
-        "Pre-Wedding Shoot",
-        "Haldi",
-        "Mehndi",
-        "Sangeet",
-      ];
+  const occasionWear = [
+    "Wedding",
+    "Pre-Wedding Shoot",
+    "Haldi",
+    "Mehndi",
+    "Sangeet",
+  ];
 
-      const menItemsType = [
-        "Sherwani",
-        "Suit",
-        "Blazer",
-        "Taxedo"
-      ];
-      
-    return (
-        <Navbar  variant="dark" expand="lg" className="custom-navbar">
-            <Container>
-                <Navbar.Brand href="#" className="mr-auto">
-                    <img
-                        src={log}
-                        width="30"
-                        height="30"
-                        className="d-inline-block logo-img  align-top"
-                        alt="Your Logo"
-                    />
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbar-nav" />
-                <Navbar.Collapse id="navbar-nav">
-                    <Nav className="mx-auto">
-                    <Categories title="Women's Wear" id="women-dropdown" womenItemsByType={womenItemsType} itemsByOccasion={occasionWear}/>
-                        <Categories title="Men's Wear" id="men-dropdown" menItemsByType={menItemsType} itemsByOccasion={occasionWear}/>
-                    </Nav>
-                    <Nav>
-                        <Nav.Link href="#" className="mr-2">
-                            <FaUser />
-                        </Nav.Link>
-                        <Nav.Link href="#" className="mr-2">
-                            <FaSearch />
-                        </Nav.Link>
-                        <Nav.Link href="#">
-                            <FaShoppingCart />
-                        </Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+  const menItems = ["Sherwani", "Suit", "Blazer", "Taxedo"];
+
+  return (
+    <Navbar variant="dark" expand="lg" className="custom-navbar">
+      <Container>
+        <Navbar.Brand href="#" className="mr-auto">
+          <img
+            src={log}
+            width="50"
+            height="50"
+            className="d-inline-block logo-img  align-top"
+            alt="Your Logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="mx-auto">
+            <Categories
+              title="Women's Wear"
+              id="women-dropdown"
+              itemsByType={womenItems}
+              itemsByOccasion={occasionWear}
+            />
+            <Categories
+              title="Men's Wear"
+              id="men-dropdown"
+              itemsByType={menItems}
+              itemsByOccasion={occasionWear}
+            />
+             <Nav.Link href="#">Customer Stories</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#" className="mr-2 custom-nav-link">
+              <FaUser />
+            </Nav.Link>
+            <Nav.Link href="#" className="mr-2 custom-nav-link">
+              <FaSearch />
+            </Nav.Link> 
+            <Nav.Link href="#" className="mr-2 custom-nav-link">
+              <FaShoppingCart />
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default NavbarComponent;
