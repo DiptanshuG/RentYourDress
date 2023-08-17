01c05c5a88d8ee9ea110e2f7ec659304d824ceb1
yashikajain0312 <yashikajain401@gmail.com>
2023-08-17 21:49:00 +0530

    minor change

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -15,7 +15,7 @@ const HomePage = () => {
             <h2 className="h2 text-black" data-aos="fade-up" data-aos-duration="1500">Custom – Character – Class</h2>
             <p className="custom-p" data-aos="fade-up" data-aos-duration="1300" data-aos-delay="500">Fashion is like eating, you shouldn't stick to the same menu.</p>
             <p className="custom-p" data-aos="fade-up"data-aos-duration="1300" data-aos-delay="500">
-              Don’t be into trends. Don’t make fashion own you, but you decide
+              Don’t be into trends, Don’t make fashion own you, but you decide
               what you are, what you want to express by the way you dress and the
               way you live.
             </p>
